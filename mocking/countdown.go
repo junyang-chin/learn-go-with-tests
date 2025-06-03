c75529b6e10716e4b5d3625a5ecd18fbc8615eef
junyang-chin <danny_cjy@hotmail.com>
2025-06-03 12:52:29 +0800

    mocks

---
+++ mocking/countdown.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"iter"
+	"os"
+	"time"
+)
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := range countDownFrom(3) {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, "Go!")
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{duration: 5 * time.Second, sleep: time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
